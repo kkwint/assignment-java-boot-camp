d291f0e49cd634f0dafa61634cbfc243adc8a5a9
Kawint <s.kawintorn@gmail.com>
2022-02-27 14:49:59 +0700

    #19 feat: add PaymentService

---
+++ src/main/java/com/week1/assignment/service/PaymentService.java
@@ -0,0 +1,44 @@
+package com.week1.assignment.service;
+
+import com.week1.assignment.entity.User;
+import com.week1.assignment.entity.UserPayment;
+import com.week1.assignment.model.PaymentDetailResponseDTO;
+import com.week1.assignment.model.UserAddressResponseDTO;
+import com.week1.assignment.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public PaymentDetailResponseDTO getDefaultPaymentDetail(String userName) {
+
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        return transformToPaymentDetailDTO(user.get());
+    }
+
+    private PaymentDetailResponseDTO transformToPaymentDetailDTO(User user) {
+        PaymentDetailResponseDTO paymentDetailResponse = new PaymentDetailResponseDTO();
+
+        UserPayment defaultUserPayment = user.getUserPaymentList()
+                .stream()
+                .filter(payment -> payment.getIsDefaultPayment().equals(true))
+                .findFirst().get();
+
+        paymentDetailResponse.setId(defaultUserPayment.getId());
+        paymentDetailResponse.setPayment_type(defaultUserPayment.getPayment_type());
+        paymentDetailResponse.setCardNo(defaultUserPayment.getCardNo());
+        paymentDetailResponse.setCardName(defaultUserPayment.getCardName());
+        paymentDetailResponse.setExpiryDate(defaultUserPayment.getExpiryDate());
+        paymentDetailResponse.setCvv(defaultUserPayment.getCvv());
+
+        return paymentDetailResponse;
+    }
+
+}
